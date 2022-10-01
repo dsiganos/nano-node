14660516244f6e0d009d9d3a8b39b77aebc96cfe
Dimitrios Siganos <dimitris@siganos.org>
2022-10-01 11:14:48 +0100

    Read TEST_USE_ROCKSDB better

--- nano/lib/rocksdbconfig.cpp
+++ nano/lib/rocksdbconfig.cpp
@@ -31,6 +31,5 @@ nano::error nano::rocksdb_config::deserialize_toml (nano::tomlconfig & toml)
 
 bool nano::rocksdb_config::using_rocksdb_in_tests ()
 {
-	auto use_rocksdb_str = std::getenv ("TEST_USE_ROCKSDB");
-	return use_rocksdb_str && (boost::lexical_cast<int> (use_rocksdb_str) == 1);
+	return nano::get_env_int_or_default ("TEST_USE_ROCKSDB", 0) == 1;
 }
